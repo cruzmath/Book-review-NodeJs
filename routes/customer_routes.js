28f188f35236c3e2884c1eb28c2a7906f940e12b
Matheus <cruzmatheus2001@gmail.com>
2025-02-26 21:35:29 -0300

    Update customer_routes.js

--- routes/customer_routes.js
+++ routes/customer_routes.js
@@ -10,22 +10,29 @@ const isValid = (username)=>{ //returns boolean
 }
 
 //only registered users can login
-regd_users.post("/customer/login", (req,res,next) => {
-    const { username, password } = req.body.user;
-    let valid_usernames = users.filter(user => user.username == username)
-    if(password !== users[users.indexOf(valid_usernames)].password) {
-        return res.json({ message: "Invalid username or password" });
-    }
-    // Generate JWT access token
-    let accessToken = jwt.sign({ username }, 'access', { expiresIn: 300 });
-    return res.status(200).send("User successfully logged in " + accessToken);
-});
+// For some reason this doen't work with me, but it should
+// regd_users.use("/customer/login", (req, res, next) => {
+//     const { username, password } = req.body.user;
+//     let valid_users = users.filter(user => user.username == username)
+//     if(password !== valid_users[0].password) {
+//         return res.json({ message: "Invalid username or password" });
+//     }
+//     // Generate JWT access token
+//     let accessToken = jwt.sign({ username }, 'access', { expiresIn: 300 });
 
+//     return res.status(200).send("User successfully logged in " + accessToken);
+// });
+
+// Middleware to verify token for authenticated routes
 regd_users.use("/", (req, res, next) => {
+    if (req.path === '/customer/login') {
+        return next();
+    }
     const token = req.headers['authorization'];
-    if(!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+    
     jwt.verify(token, 'access', (err, decoded) => {
-        if(err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.', err });
+        if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.', err });
         next();
     });
 });
@@ -37,5 +44,5 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 });
 
 module.exports.authenticated = regd_users;
-module.exports.isValid = isValid;
+// module.exports.isValid = isValid;
 module.exports.users = users;
