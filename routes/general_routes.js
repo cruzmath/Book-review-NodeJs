f2cee430a2f12d5bc0c647e515b5478a43631cf4
Matheus <cruzmatheus2001@gmail.com>
2025-02-28 09:44:31 -0300

    Update general_routes.js
    Using Promises de upgrade the efficiency of the processes

--- routes/general_routes.js
+++ routes/general_routes.js
@@ -4,55 +4,63 @@ const public_users = express.Router();
 let customers = require('./customer_routes').users;
 
 public_users.post("/register", (req,res) => {
-    const{ username, password} = req.body.user
+    const{ username, password} = req.body.user;
     customers.push({username, password});
     if(!username || !password) res.send('No username or password provided')
-    res.send("User created successfully")
+    res.send("User created successfully");
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify({ books }, null, 4));
+    const list_books = new Promise((resolve, _) => resolve(res.send(JSON.stringify({ books }, null, 4))));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    let isbn = req.params.isbn;
-    if( books[isbn] ) res.send(JSON.stringify(books[isbn], null, 4));
-    else res.send('No book found');
+    const get_isbn = new Promise((resolve, rej) => {
+        let isbn = req.params.isbn;
+        if( books[isbn] ) resolve(res.send(JSON.stringify(books[isbn], null, 4)));
+        else rej(res.send('No book found'));
+    });
 });
 
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    let author = req.params.author;
-    let filtered_books = [];
-    for(let i = 1; i <= Object.keys(books).length; i++){
-        book = books[i]
-        if( book.author == author ){
-            filtered_books.push(book);
-        };
-    }
-    res.send(JSON.stringify(filtered_books, null, 4));
+    const get_author = new Promise((resolve, rej) => {
+        let author = req.params.author;
+        let filtered_books = [];
+        for(let i = 1; i <= Object.keys(books).length; i++){
+            let book = books[i];
+            if( book.author == author ){
+                filtered_books.push(book);
+            }
+        }
+        resolve(res.send(JSON.stringify(filtered_books, null, 4)));
+    });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    let title = req.params.title;
-    let filtered_books = [];
-    for(let i = 1; i <= Object.keys(books).length; i++){
-        book = books[i];
-        if(book.title == title){
-            filtered_books.push(book);
+    const get_title = new Promise((resolve, _) => {
+        let title = req.params.title;
+        let filtered_books = [];
+        for(let i = 1; i <= Object.keys(books).length; i++){
+            let book = books[i];
+            if(book.title == title){
+                filtered_books.push(book);
+            }
         }
-    }
-    res.send(JSON.stringify(filtered_books, null, 4));
+        resolve(res.send(JSON.stringify(filtered_books, null, 4)));
+    });
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-    let isbn = req.params.isbn;
-    let reviews = books[isbn].reviews;
-    res.send(JSON.stringify( reviews, null, 4));
+    const get_review = new Promise((resolve, _) => {
+        let isbn = req.params.isbn;
+        let reviews = books[isbn].reviews;
+        resolve(res.send(JSON.stringify( reviews, null, 4)));
+    });
 });
 
 module.exports=public_users;
