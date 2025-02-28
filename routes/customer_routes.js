7632ae27894ecc6b39b324db111b7a26d7c97682
Matheus <cruzmatheus2001@gmail.com>
2025-02-28 09:45:23 -0300

    Correction of errors and adding new features.

--- routes/customer_routes.js
+++ routes/customer_routes.js
@@ -5,44 +5,68 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
-
-//only registered users can login
-// For some reason this doen't work with me, but it should
-// regd_users.use("/customer/login", (req, res, next) => {
-//     const { username, password } = req.body.user;
-//     let valid_users = users.filter(user => user.username == username)
-//     if(password !== valid_users[0].password) {
-//         return res.json({ message: "Invalid username or password" });
-//     }
-//     // Generate JWT access token
-//     let accessToken = jwt.sign({ username }, 'access', { expiresIn: 300 });
-
-//     return res.status(200).send("User successfully logged in " + accessToken);
-// });
-
-// Middleware to verify token for authenticated routes
-regd_users.use("/", (req, res, next) => {
-    if (req.path === '/customer/login') {
-        return next();
+// only registered users can login
+regd_users.use("/login", (req,res,next) => {
+    const { username, password } = req.body.user;
+    let valid_users = users.filter(user => user.username.includes(username));
+    if(password !== valid_users[0].password) {
+        return res.json({ message: "Invalid username or password" });
     }
+    // Generate JWT access token
+    let accessToken = jwt.sign({ username }, 'access', { expiresIn: 3600 });
+    return res.status(200).send("User successfully logged in " + accessToken);
+});
+
+// Verify user's token before proceding
+regd_users.use("/", (req, res, next) => {
     const token = req.headers['authorization'];
-    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
-    
+    if(!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+
     jwt.verify(token, 'access', (err, decoded) => {
-        if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.', err });
+        if(err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.', err });
         next();
     });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const token = req.headers['authorization'];
+    const decoded = jwt.verify(token, 'access');
+    let username = decoded.username;
+    const review = req.query.review;
+    const isbn = req.params.isbn;
+
+    if(!books[isbn]) res.send('No book found');
+    let valid_users = books[isbn].reviews.filter(book => Object.keys(book).includes(username))[0];
+    if(!valid_users) { // Create a new review
+        const user = {};
+        user[username] = review;
+        books[isbn].reviews.push(user);
+        res.send(`${username} review's added for ${books[isbn].title}`);
+    } else { // Modify an existing review
+        review_index = books[isbn].reviews.indexOf(valid_users);
+        books[isbn].reviews[review_index][username] = review;
+        res.send(`${username} review's updated for ${books[isbn].title}`)
+    }
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const token = req.headers['authorization'];
+    const decoded = jwt.verify(token, 'access');
+    let username = decoded.username;
+    const isbn = req.params.isbn;
+
+    if(!books[isbn]) res.send('No book found');
+    let valid_users = books[isbn].reviews.filter(book => Object.keys(book).includes(username))[0];
+    review_index = books[isbn].reviews.indexOf(valid_users);
+    if(!valid_users) {
+        res.send(`No review found for ${username} in ${books[isbn].title}`);
+    } else {
+        books[isbn].reviews.splice(review_index, 1);
+        res.send(`${username} review's deleted for ${books[isbn].title}`);
+    }
 });
 
 module.exports.authenticated = regd_users;
-// module.exports.isValid = isValid;
 module.exports.users = users;
