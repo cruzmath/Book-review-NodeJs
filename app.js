a98e0104ced13ff537e4fd231c0ce99beaf862d0
Matheus <cruzmatheus2001@gmail.com>
2025-02-28 09:46:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const customer_routes = require('./routes/customer_routes').authenticated;
 const general_routes = require('./routes/general_routes');
-let users = require('./routes/customer_routes').users;
 const PORT = 8080;
 
 const app = express();
 app.use(express.json());
-
-app.use("/customer/login", (req, res, next) => {
-    const { username, password } = req.body.user;
-    let valid_users = users.filter(user => user.username == username)
-    if(password !== valid_users[0].password) {
-        return res.json({ message: "Invalid username or password" });
-    }
-    let accessToken = jwt.sign({ username }, 'access', { expiresIn: 300 });
-
-    return res.status(200).send("User successfully logged in and has the following access token " + accessToken);
-});
 
 app.use("/customer", customer_routes);
 app.use("/", general_routes);
